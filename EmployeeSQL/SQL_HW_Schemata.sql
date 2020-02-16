38024133a56b9a03277edc4757881a1169bfaefb
Leticia Vaz <40251163+LeticiaVaz@users.noreply.github.com>
2020-02-16 16:04:02 -0500

    employees data

---
+++ EmployeeSQL/SQL_HW_Schemata.sql
@@ -0,0 +1,47 @@
+dept_manager
+---
+dept_no varchar FK >- departments.dept_no
+emp_no int FK >- employees.emp_no
+from_date date
+to_date date
+
+departments
+---
+dept_no varchar PK 
+dept_name varchar
+
+
+dept_emp
+---
+emp_no int FK >- employees.emp_no
+dept_no varchar FK >- departments.dept_no
+from_date date
+to_date date
+
+employees
+---
+emp_no PK int
+birth_date date
+first_name varchar
+last_name varchar
+gender varchar
+hire_date date
+
+salaries
+---
+emp_no int FK >- employees.emp_no
+salary double
+from_date date
+to_date date
+
+titles
+---
+emp_no int FK >- employees.emp_no
+title varchar
+from_date date
+to_date date
+
+
+
+
+
